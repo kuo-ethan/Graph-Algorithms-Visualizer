42f8f7d39bd64faa34a80090726646e1fc9805d4
Ethan Kuo <ek0335@berkeley.edu>
2022-08-22 02:40:48 -0700

    Starting prims

---
+++ src/algorithms/prims.js
@@ -0,0 +1,8 @@
+import {PriorityQueue} from './priority_queue';
+
+/* Prim's algorithm for minimum spanning trees.
+Returns a list of nodes in order of visitation.
+*/
+export function prims(grid, startNode) {
+    const visitedNodesInOrder = []; // for animation purposes
+}
